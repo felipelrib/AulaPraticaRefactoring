5c3a075379d50760f54383f0e66510dae27c8bec
Felipe Ribeiro <felipelrib@gmail.com>
2020-10-21 14:12:00 -0300

    Commit 8

--- Customer.java
+++ Customer.java
@@ -5,7 +5,7 @@ public class Customer {
     private String _name;
     private Vector _rentals = new Vector();
 
-    public Customer (String name){
+    public Customer(String name) {
         _name = name;
     }
 
@@ -13,8 +13,26 @@ public class Customer {
         _rentals.addElement(arg);
     }
 
-    public String getName (){
+    public String getName() {
         return _name;
+    }
+
+    public String htmlStatement() {
+        Enumeration rentals = _rentals.elements();
+        String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            // show figures for each rental
+            result += each.getMovie().getTitle()+ ": " +
+                String.valueOf(each.getCharge()) + "<BR>\n";
+        }
+
+        // add footer lines
+        result += "<P>You owe <EM>" + String.valueOf(getTotalCharge()) + "</EM><P>\n";
+        result += "On this rental you earned <EM>" +
+            String.valueOf(getTotalFrequentRenterPoints()) +
+            "</EM> frequent renter points<P>";
+        return result;
     }
 
     public String statement() {
@@ -44,7 +62,7 @@ public class Customer {
         return result;
     }
 
-     private int getTotalFrequentRenterPoints(){
+    private int getTotalFrequentRenterPoints() {
         int result = 0;
         Enumeration rentals = _rentals.elements();
         while (rentals.hasMoreElements()) {
